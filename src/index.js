98c49b37a16e644a043812c1fbd80c3df97d724e
SergeyYakimov <teriertis@yandex.ru>
2021-03-12 23:32:51 +0800

    feat: make task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        for (let j = 0; j < str.length; j++) {
+            if (
+                str[j] === bracketsConfig[i][0] &&
+                str[j + 1] === bracketsConfig[i][1]
+            ) {
+                let newStr = str.substring(0, j) + str.substring(j + 2);
+                return check(newStr, bracketsConfig);
+            }
+        }
+    }
+
+    if (str === "") {
+        return true;
+    }
+
+    return false;
+};
